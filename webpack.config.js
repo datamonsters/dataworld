d9c0f96b1ee40e046abfc2a80eda57617833b7bf
Gleb YP <dev@gleb.pw>
2016-08-03 12:56:15 +0300

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 var path = require('path');
-var AssetsPlugin = require('assets-webpack-plugin');
-var assetsPluginInstance = new AssetsPlugin();
 
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || "8888";
